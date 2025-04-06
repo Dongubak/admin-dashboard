75e07c4d74c627369ba3dd8fe78ae7ff898a563f
Dongubak <91736442+Dongubak@users.noreply.github.com>
2025-04-06 16:25:15 +0900

    token logic 수정
    token logic 수정

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,22 +1,22 @@
 export const setAuthData = (token: string, refreshToken: string, user: object) => {
   localStorage.setItem('token', token);
-  localStorage.setItem('refreshToken', refreshToken);
+  localStorage.setItem('RT', refreshToken);
   localStorage.setItem('user', JSON.stringify(user));
 };
 
 export const getAuthData = () => {
   const token = localStorage.getItem('token');
-  const refreshToken = localStorage.getItem('refreshToken');
+  const RT = localStorage.getItem('RT');
   const user = localStorage.getItem('user');
   return {
     token,
-    refreshToken,
+    RT,
     user: user ? JSON.parse(user) : null,
   };
 };
 
 export const clearAuthData = () => {
   localStorage.removeItem('token');
-  localStorage.removeItem('refreshToken');
+  localStorage.removeItem('RT');
   localStorage.removeItem('user');
 };
